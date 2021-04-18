951ab12a318cc28d0f03c159b81e900e4b0e182f
edlee <edlee@synapsoft.co.kr>
2021-04-18 18:14:05 +0900

    시작

---
+++ src/ch1_recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+ * 
+ * 
+ */
+class Solution {
+public:    
+
+    int dummy() {
+        return 123;
+    }
+};
+
+
+// Driver code
+int main() {
+	Solution sol;
+  	
+	cout << "ans = " << sol.dummy();
+	
+	return 0;
+}
